aae682f787cf8b1de03311eb7c05ebc056580628
Ali Safarli <7n0mn2a@code.edu.az>
2023-03-08 20:43:52 +0400

    added

---
+++ HomeWork_2/HomeWork_2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Task 1
+//int[] arr = { 12, 13, 25, 50 };
+//int sum = 0;
+//for (int i = 0; i <arr.Length; i++){
+//	sum += arr[i];
+//}
+//			Console.WriteLine(sum);
+
+//Task 2
+//string[] words = { "salam", "Sagol", "alma", "pandemonium" };
+//for (int i = 0; i < words.Length; i++)
+//{
+//    if (words[i].Length > 4)
+//    {
+//        Console.WriteLine(words[i] + "");
+//    }
+//}
+
+//Task 3
+//int[] nums = new int[] { 1, 22, 33, 44, 5 };
+//int count = 0;
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] < 10)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+//Task 4
+//int[] nums = new int[] { 1, 22, 33, 44, 5 };
+//int[] nums2 = new int[] { 22, 3, 44, 42 };
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    for (int j = 0; j < nums2.Length; j++)
+//    {
+//        if (nums[i] == nums2[j])
+//        {
+//            Console.WriteLine(nums[i] + " ");
+//        }
+//    }
+//}
